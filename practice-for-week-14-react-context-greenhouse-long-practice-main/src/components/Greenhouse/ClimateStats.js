e1687df2bb7838781e170f4e25716964c5aecd7e
Dan Holodak <danholodak@gmail.com>
2023-03-22 14:32:53 -0400

    bonus 1

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/ClimateStats.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/ClimateStats.js
@@ -1,12 +1,27 @@
 import './ClimateStats.css';
 import { useClimate } from '../../context/ClimateContext';
+import { useEffect, useState } from 'react';
 
 function ClimateStats() {
 const {humidity, temperature} = useClimate();
+let [temp, setTemp] = useState(50)
+
+ useEffect(() => {
+  
+    if (temp < temperature){
+      
+      setTimeout(()=>{setTemp(temp+1)}, 1000)
+      console.log(temp)
+    }else if(temp > temperature){
+      setTimeout(()=>{setTemp(temp-1)}, 1000)
+  }
+
+}, [temperature, temp]);
+
   return (
     <div className="climate-stats">
       <div className="temperature">
-        Temperature {temperature}°F
+        Temperature {temp}°F
       </div>
       <div className="humidity">
         Humidity {humidity}%
