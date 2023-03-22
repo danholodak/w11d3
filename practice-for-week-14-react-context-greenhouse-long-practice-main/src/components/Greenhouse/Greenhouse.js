1bfc7d2840cb608fc72a07fa45cdba37ab3a89a2
Dan Holodak <danholodak@gmail.com>
2023-03-22 11:54:40 -0400

    part 1

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Greenhouse/Greenhouse.js
@@ -4,13 +4,16 @@ import './Greenhouse.css';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
+import { useTheme } from '../../context/ThemeContext';
 
 function Greenhouse() {
+ const {themeName} = useTheme();
+ const src = themeName==="day"? dayImage : nightImage
 
   return (
     <section>
       <img  className='greenhouse-img'
-            src={dayImage}
+            src={src}
             alt='greenhouse' 
       />
       <LightSwitch />
